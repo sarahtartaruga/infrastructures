57c957caece8181486ae9a07241ecc2558fce98e
sarahtartaruga <fannyFlye@gmx.de>
2020-11-12 14:20:25 +0100

    logo spin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './img/IMG_2184.png';
+import background from './img/IMG_2186.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{backgroundImage: `url(${background})`}}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Surveillance cultures - infrastructures
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
