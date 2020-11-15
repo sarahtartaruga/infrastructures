7f5ef9fd2105493044bf6d07eb4932f69925eeb7
sarahtartaruga <fannyFlye@gmx.de>
2020-11-15 12:03:46 +0100

    function to class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React from 'react';
 import logo from './img/IMG_2184.png';
 import background from './img/IMG_2186.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" style={{backgroundImage: `url(${background})`}}>
-        <p className="p-title">
-          SURVEILLANCE CULTURES INFRASTRUCTURES
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" style={{backgroundImage: `url(${background})`}}>
+          <p className="p-title">
+            SURVEILLANCE CULTURES INFRASTRUCTURES
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
